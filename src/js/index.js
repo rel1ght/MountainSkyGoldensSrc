a8f766aa8c70c25552e58779082cb08884481b8c
Tyler Manion <relytmanion@gmail.com>
2020-03-21 12:11:02 -0500

    instagram done

--- src/js/index.js
+++ src/js/index.js
@@ -3,26 +3,53 @@ import "./imports.js";
 //import instagramPosts from "./instagram-posts.js";
 //const instagramPosts = require("./instagram-posts");
 
-//const instaObj = require("instagram-basic-data-scraper-with-username");
-//const fetch = require("node-fetch");
-// get "https://api.instagram.com/oembed?url=https://www.instagram.com/p/fA9uwTtkSN/"
 main();
 async function instagrabber(url) {
-	let response;
-	await fetch(`https://api.instagram.com/oembed?url=${url}`)
-		.then(res => res.json())
-		.then(json => (response = json));
-	return response.html;
+	return new Promise(async (resolve, reject) => {
+		await fetch(url)
+			.then(res => res.json())
+			.then(json => {
+				//console.log("jsonhtml: ", json.html);
+				resolve(json.html);
+			});
+	});
 }
 
 async function main() {
-	let url = "https://www.instagram.com/p/fA9uwTtkSN/";
-	let html = await instagrabber(url);
-	console.log("html1", html);
-	document.getElementById("insta").innerHTML = html;
+	let user = await instaUserGrabber("ellies_golden_years");
+	let postsArray = await jsonDestructor(user);
+	let htmlArray = await postsHTMLGenerator(postsArray);
+	console.log("main htmlarray: ", htmlArray);
+	let response = await htmlInjector(htmlArray);
 	window.instgrm.Embeds.process();
-	console.log(await instaUserGrabber("ellies_golden_years"));
-	//console.log(await instagramPosts("cats_of_instagram"));
+}
+
+async function htmlInjector(htmlArray) {
+	let injectSpots = document.getElementsByClassName("insta");
+	for (let i = 0; i < injectSpots.length; i++) {
+		injectSpots[i].innerHTML = htmlArray[i];
+	}
+}
+
+async function postsHTMLGenerator(PostsArray) {
+	console.log("postsArray: ", PostsArray);
+	let urlArray = [];
+	let htmlArray = [];
+	let promises = [];
+	for (let i = 0; i < PostsArray.length; i++) {
+		let post = PostsArray[i];
+
+		urlArray[
+			i
+		] = `https://api.instagram.com/oembed?url=https://www.instagram.com/p/${post.node.shortcode}/`;
+		promises.push(instagrabber(urlArray[i]));
+	}
+	await Promise.all(promises).then(results => {
+		//console.log("finished: ", results);
+		htmlArray = results;
+		//console.log("htmlArray: ", htmlArray);
+	});
+	return htmlArray;
 }
 
 async function instaUserGrabber(url) {
@@ -31,4 +58,9 @@ async function instaUserGrabber(url) {
 		.then(res => res.json())
 		.then(json => (response = json));
 	return response;
+}
+async function jsonDestructor(user) {
+	//console.log("posts: ", user.graphql.user.edge_owner_to_timeline_media.edges);
+	let postsArray = user.graphql.user.edge_owner_to_timeline_media.edges;
+	return postsArray;
 }
